7dcc02e409b3f7af8ea60a8bfe3880cb45c44b49
Sachin Singh <imsachinsingh00@gmail.com>
2025-03-27 17:47:40 +0530

    initia commit

---
+++ tests/test_operations.py
@@ -0,0 +1,13 @@
+from src.math_operations import *
+
+def test_add():
+  assert add(2,3) == 5
+
+def est_sub():
+  assert sub(2,3) == -1
+
+def test_mul():
+  assert mul(2,3) == 6
+
+def test_div():
+  assert div(10,5) == 2
